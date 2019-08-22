ece72fc1c728686ad61e82073144b2f2f3a0ad72
pplchk <proton-team@hotmail.com>
2019-08-22 20:25:11 +0800

    add

--- src/App.js
+++ src/App.js
@@ -17,10 +17,18 @@ var dateUtil = require('./modules/utils/dateUtil.js');
 
 var $ = require('jQuery');
 
+$(window).scroll(function(){
+	if($(".navbar").offset().top > 50){
+		$('.navbar-fixed-top').addClass('top-nav');
+	}else{
+		$('.navbar-fixed-top').removeClass('top-nav');
+	}
+});
+
 $(function(){
 	let d = new Date();
 	let ds = dateUtil.format(d,"yyyyMMdd hh:mm");
 	console.log(ds);
-	$("p.date-container").text(ds);
+	//$("p.date-container").text(ds);
 });
 
