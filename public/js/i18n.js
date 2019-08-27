f5869b908558c4752d3747c430137699fff70812
BIGPango <developer-team@tuto.io>
2019-08-27 13:17:14 +0800

    Don't do useless work

--- public/js/i18n.js
+++ public/js/i18n.js
@@ -12,10 +12,12 @@
  */
 
 const CFG = {
-lang : "en",
-selector : {
+  lang : "en",
+  selector : {
 	selChinese : "chinese"
-}
+  },
+  slogan_cn:'<h1 class="mx-auto my-0 text-slogan pt-lg-5 pb-lg-3 i18n" id="slogan" data-en="Traffic usage form" data-cn="流量使用形式"> 流量使用形式 </h1>',
+  slogan_en:'<h1 class="mx-auto my-0 text-slogan pt-lg-5 pb-lg-3 i18n" id="slogan" data-en="Traffic usage form" data-cn="流量使用形式"> Traffic usage form </h1>'
 };
 
 function InitializeI18n(){
@@ -27,15 +29,24 @@ function InitializeI18n(){
     let lg = $(this).data('sign');
     $(this).addClass('lang-active');
     setLang(lg);
+    $('header').addClass('masthead');
   });
   setLang(CFG.lang);
 } 
 
 function setLang(lg){
 	$('.i18n').each((index,el)=>{
+
 		var text = $(el).data(''+lg);
+		
 		if(text){
-			$(el).text(text);
+			if(el.id=="slogan"){
+				let $parent = $(el).parent();			
+				$(el).remove();
+				lg=='en' ? $parent.prepend(CFG.slogan_en) : $parent.prepend(CFG.slogan_cn);
+			}else{
+				$(el).text(text);
+			}
 		}
 		let url = $(el).data(lg+'url');
 		if(url){
