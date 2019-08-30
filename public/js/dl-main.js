c9d74f13f3941a98b632b5cb2e7ba1f053a3535f
BIGPango <developer-team@tuto.io>
2019-08-30 20:31:06 +0800

    download completed

--- public/js/dl-main.js
+++ public/js/dl-main.js
@@ -12,7 +12,85 @@
  * https://www.ppn.one
  *
  */
-(function($){
+const CFG = {
+  lang : "en",
+  selector : {
+	selChinese : "chinese"
+  },
+
+  github : {
+  	cssSelector:".GithubDownload",
+  	pubBase:"https://github.com/BIGPango/pangolink/releases/download",
+  	pubVersion:"v1.2",
+  	pubFile:"pangolin-release.apk",
+  	wxBrowser:"wx_dl_cn.html"
+  },
+  getGithubHref:function(type){
+  	return type ? CFG.wxBrowser :  CFG.github.pubBase+'/' + CFG.github.pubVersion + "/" + CFG.github.pubFile;
+  }
+};
+
+var DownloadMain = {
+  isWxBrowser:false,
+  isQQInstall:false,
+  isQQBrowser:false,
+  slogan_cn:'<h1>免费下载</h1>',
+  slogan_en:'<h1>Download For Free</h1>',
+  initBrowserType : function(){
+  	//console.log("init DownloadMain >>>>>>");
+	var ua = navigator.userAgent.toLowerCase();
+	if(ua.indexOf(' qq')>-1 && ua.indexOf('mqqbrowser') <0 ){
+		//inner QQ
+		DownloadMain.isQQInstall = true;
+		return;
+	}
+
+	if(ua.indexOf('mqqbrowser') >-1 && ua.indexOf(' qq')<0){
+		DownloadMain.isQQBrowser = true;
+		return;
+	}
+
+	if(ua.match(/MicroMessenger/i) == 'micromessenger'){
+		DownloadMain.isWxBrowser = true;
+		return;
+	} 	
+  },
+  isWxBrowser:function(){
+  	//console.log(">>>>>>");
+  	return DownloadMain.isWxBrowser || DownloadMain.isQQInstall || DownloadMain.isQQBrowser;
+  },
+  i18nEvent:function(){
+  	$('.chinese').on('click',function(){
+      $(".chinese").removeClass('lang-active');
+      let lg = $(this).data('sign');
+      $(this).addClass('lang-active');
+      DownloadMain.setLang(lg);
+      $('header').addClass('masthead');
+  	});
+
+  	$('p.qrcode-wallet').on('click',function(){
+  	  $('div.qrcode-wallet-img').hasClass('d-none') ? $('div.qrcode-wallet-img').removeClass('d-none') :$('div.qrcode-wallet-img').addClass('d-none');
+  	});
+  },
+  setLang:function(lg){
+  	$('.i18n').each((index,el)=>{
+  	  let text = $(el).data(''+lg);
+
+  	  if(text){
+  	  	$(el).text(text);
+  	  }
+  	  if(el.id=="mastTitle"){
+  	  	console.log(el.id);
+  	  }
+  	});
+
+/*  	$('div.text-slogan').find('h1').remove();
+  	$('div.text-slogan').html(lg == 'en' ? DownloadMain.slogan_en : DownloadMain.slogan_en);*/
+  }
+};
+
+
+(function($,cfg,dlm){
   "use strict";
   $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
     if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
@@ -49,5 +127,12 @@
   // Collapse the navbar when page is scrolled
   $(window).scroll(navbarCollapse);
 
+  /**
+   * lang 
+   */
+   dlm.initBrowserType();
+   dlm.i18nEvent();
 
-})(jQuery); 
+   let type = dlm.isWxBrowser();
+
+})(jQuery,CFG,DownloadMain); 
