6359f07d2e8600956c4f8b4434f6b65d811705b5
BIGPango <developer-team@tuto.io>
2019-08-30 16:49:53 +0800

    Download

--- public/js/dl-main.js
+++ public/js/dl-main.js
@@ -12,3 +12,42 @@
  * https://www.ppn.one
  *
  */
+(function($){
+  "use strict";
+  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: (target.offset().top - 70)
+        }, 1000, "easeInOutExpo");
+        return false;
+      }
+    }
+  });
+
+  $('.js-scroll-trigger').click(function() {
+    $('.navbar-collapse').collapse('hide');
+  });  
+
+  $('body').scrollspy({
+    target: '#mainNav',
+    offset: 100
+  });
+
+
+  var navbarCollapse = function() {
+    if ($("#mainNav").offset().top > 100) {
+      $("#mainNav").addClass("navbar-shrink");
+    } else {
+      $("#mainNav").removeClass("navbar-shrink");
+    }
+  };
+  // Collapse now if page is not at top
+  navbarCollapse();
+  // Collapse the navbar when page is scrolled
+  $(window).scroll(navbarCollapse);
+
+
+})(jQuery); 
